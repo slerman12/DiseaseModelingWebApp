7eb6b63fc04a17fbc737bb5ced2096ea23b260b9
slerman12 <slerman@u.rochester.edu>
2018-03-20 21:47:01 -0400

    Created form and model

--- model.py
+++ model.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
     model_name = 'saved_models/model_future_severity_untreated_off_SCORE_FUTURE_UPDRS_III_0.001_ranking_PD_GENPD_REGPD_0_to_0.33_timeframe'
     X = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
-    loaded_model = pickle.load(open(model_name, 'rb'))
-
-    pred = loaded_model.predict(X)
-
-    print(pred)
+    # loaded_model = pickle.load(open(model_name, 'rb'))
+    #
+    # pred = loaded_model.predict(X)
+    #
+    # print(pred)
 
     app.run(host='0.0.0.0', port=5001)
